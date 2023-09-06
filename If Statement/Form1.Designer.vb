ce4f94988347d21a57d2f41fbe12bdb7a1f0bd2a
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-06 13:38:12 +0200

    Add project files.

---
+++ If Statement/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGreetings = New System.Windows.Forms.Button()
+        Me.lblCountryName = New System.Windows.Forms.Label()
+        Me.txtCountryName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnGreetings
+        '
+        Me.btnGreetings.Location = New System.Drawing.Point(112, 149)
+        Me.btnGreetings.Name = "btnGreetings"
+        Me.btnGreetings.Size = New System.Drawing.Size(159, 65)
+        Me.btnGreetings.TabIndex = 0
+        Me.btnGreetings.Text = "Greetings"
+        Me.btnGreetings.UseVisualStyleBackColor = True
+        '
+        'lblCountryName
+        '
+        Me.lblCountryName.AutoSize = True
+        Me.lblCountryName.Location = New System.Drawing.Point(58, 52)
+        Me.lblCountryName.Name = "lblCountryName"
+        Me.lblCountryName.Size = New System.Drawing.Size(108, 20)
+        Me.lblCountryName.TabIndex = 1
+        Me.lblCountryName.Text = "Country name"
+        '
+        'txtCountryName
+        '
+        Me.txtCountryName.Location = New System.Drawing.Point(184, 49)
+        Me.txtCountryName.Name = "txtCountryName"
+        Me.txtCountryName.Size = New System.Drawing.Size(100, 26)
+        Me.txtCountryName.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtCountryName)
+        Me.Controls.Add(Me.lblCountryName)
+        Me.Controls.Add(Me.btnGreetings)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGreetings As Button
+    Friend WithEvents lblCountryName As Label
+    Friend WithEvents txtCountryName As TextBox
+End Class
---
+++ If Statement/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGreetings.Click
+        'Declaring variables
+        Dim strCountryName As String
+
+        strCountryName = txtCountryName.Text.ToUpper
+
+        'Implementing an IF statement
+        If strCountryName = "SOUTH AFRICA" Then
+            MessageBox.Show("Molo!")
+        ElseIf strCountryName = "FRANCE" Then
+            MessageBox.Show("Bonjour!")
+        ElseIf strCountryName = "SPAIN" Then
+            MessageBox.Show("Ola!")
+        ElseIf strCountryName = "RUSSIA" Then
+            MessageBox.Show("Zdravstvuyte!")
+        ElseIf strCountryName = "CHINA" Then
+            MessageBox.Show("Nin Hao!")
+        ElseIf strCountryName = "GERMANY" Then
+            MessageBox.Show("Guten Tag!")
+        Else
+            MessageBox.Show("Hello!")
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtCountryName.TextChanged
+
+    End Sub
+End Class
